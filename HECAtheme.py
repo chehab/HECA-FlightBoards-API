b41fc616f51fe1daec929c67fbc9b5978bba3f57
Chehab Mustafa-Hilmy <m@chehab.me>
2013-02-28 18:19:36 +0100

    [NEW] Add metatag to the theme's header, add html refrash tag

--- HECAtheme.py
+++ HECAtheme.py
@@ -5,19 +5,22 @@ import sys
 class HECATheme:
 
     
-    __sectionA = None
-    __sectionB = None
-    __alert    = None
-    __page     = None
+    __sectionA  = None
+    __sectionB  = None
+    __alert     = None
+    __page      = None
+
+    __head_lst__ = []
     
     def echo(self):
+        self.__cat_all__()
         if self.__alert:
             self.__page = self.__sectionA + self.__alert + self.__sectionB
         else:
             self.__page = self.__sectionA + self.__sectionB
         return self.__page
     
-    def __init__(self):
+    def __cat_all__(self):
         #A
         self._genHead()
         self._genNavBar()
@@ -30,6 +33,11 @@ class HECATheme:
         self.__sectionA = "Content-type: text/html\r\n"
         self.__sectionA += "Content-Encoding: UTF8\r\n\r\n"
         self.__sectionA += self.__loadfile("theme/0-head.html")
+        
+        for mt in __head_lst__:
+            self.__sectionA += mt
+
+        self.__sectionA += "  </head>"
     
     def _genNavBar(self):
         self.__sectionA += self.__loadfile("theme/1-navbar.html")
@@ -39,6 +47,14 @@ class HECATheme:
         self.__alert += self.__loadfile("theme/2-alert.html").format( \
         type=type, title=title, msg=msg )
     
+    def pageRefresh(seconds, url=None):
+        url = ";url="+url if url else ""
+        metatag = '<meta http-equiv="refresh" content="{}{}">'
+        self.__head_lst__.append( metatag.format(seconds,url) )
+
+    def addMetaTag(htmltag):
+        self.__head_lst__.append( htmltag )
+
     def _genHeroUnit(self):
         self.__sectionB = ""
         self.__sectionB += self.__loadfile("theme/3-hero-unit.html")
